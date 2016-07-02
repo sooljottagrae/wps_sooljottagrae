40ee55aac7450c051e39525a2b647c6735488be4
alexKwonIsAwesome <khwalex@gmail.com>
2016-07-02 14:48:07 +0900

    init `posts`, `users` and `tags` applications:
    Initiated posts, users and tags apps then added to INSTALLED_APPS in
    base.py settings. Also created /models/ direcotry in each application
    directory since the models have to be created eventually.

--- sooljottagrae/sooljottagrae/settings/partials/base.py
+++ sooljottagrae/sooljottagrae/settings/partials/base.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'users',
+    'posts',
+    'tags',
 ]
 
 MIDDLEWARE_CLASSES = [
