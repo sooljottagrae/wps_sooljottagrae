d9720823d673095b121ebe81025914edcf271d69
feanar729 <feanar729@gmail.com>
2016-07-05 21:29:55 +0900

    init user models, templates: init users, views, templates models

--- sooljottagrae/sooljottagrae/urls.py
+++ sooljottagrae/sooljottagrae/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from users.views import *
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^signup/', SignupView.as_view(), name="signup"),
+    url(r'^login/', LoginView.as_view(), name="login"),
 ]
