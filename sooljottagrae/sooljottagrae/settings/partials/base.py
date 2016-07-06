f220b9ad87e6665878a0bfc6e562f3bb952a0861
alexKwonIsAwesome <khwalex@gmail.com>
2016-07-06 14:31:02 +0900

    update settings in base.py:
    Failed to meet pep8 test on travis. Edited the code that occured as
    error as pep8 standard.

--- sooljottagrae/sooljottagrae/settings/partials/base.py
+++ sooljottagrae/sooljottagrae/settings/partials/base.py
@@ -90,15 +90,6 @@ DATABASES['default'] = dj_database_url.config(
         default=os.environ.get("DATABASE_URL"),
 )
 
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
