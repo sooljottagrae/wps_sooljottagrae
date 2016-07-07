7fea92ef0a1be0960e2d09ea08eb287ec8a2c0b0
alexKwonIsAwesome <khwalex@gmail.com>
2016-07-08 00:08:44 +0900

    init rest_framework_jwt urls:
    According to the djangorestframework-jwt document, it says to connect
    specific urls to each token functions.
    
    The /api/token/obtain/ to obtain_jwt_token is for authentication when
    the request is made as POST method and returns the user's unique token.
    This it deemed as login process. Since the token is returned, iOS client
    should store this token for further authentication.
    
    The /api/token/verity/ to verity_jwt_token is for validating the user's
    authority. When the token is sent and the server checks the validity of
    the user and gives back only the authorized resources.
    
    The /api/token/refresh/ to refresh_jwt_token is for refreshign the
    token value. Because server expires the toeken of the user,so user may not be able to authenticate and verified with formerly given toekn. This function is to refresh and create a branch new token for a user.

--- sooljottagrae/apis/urls/__init__.py
+++ sooljottagrae/apis/urls/__init__.py
@@ -1,10 +1,13 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from apis.views import *
+
 urlpatterns = [
         url(r'^posts/$', PostListAPIView.as_view(), name="postlist"),
         url(r'^posts/(?P<pk>\w+)/$', PostDetailAPIView.as_view(), name="postdetail"),
 
         url(r'^users/$', UserListAPIView.as_view(), name="userlist"), 
         url(r'^users/(?P<pk>\w+)/$',UserDetailAPIView.as_view(), name="userdetail"),
+        
+        url(r'token/', include('apis.urls.tokens', namespace="tokens")),
         ]
