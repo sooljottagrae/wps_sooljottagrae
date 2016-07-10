225c6aa19480b4d7d5e19583556fb6cc0407961c
alexKwonIsAwesome <khwalex@gmail.com>
2016-07-10 20:08:29 +0900

    edit files to meet pep8 rules:
    edited permissions.py file

--- sooljottagrae/apis/permissions.py
+++ sooljottagrae/apis/permissions.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsOwnerOrReadOnly(BasePermission):
     message = "해당 내용의 작성자만 가능합니다."
-    my_safe_method = ['GET','PUT']
+    my_safe_method = ['GET', 'PUT']
 
     def has_permission(self, request, view):
         if request.method in self.my_safe_method:
@@ -18,5 +18,6 @@ class IsOwnerOrReadOnly(BasePermission):
 """
 referred cfr lecture blog api with django rest framework.
 
-Its commit - https://github.com/codingforentrepreneurs/Blog-API-with-Django-Rest-Framework/commit/76eb764398628691a2be59bc87a541995913e2d7
+Its commit page
+https://github.com/codingforentrepreneurs/Blog-API-with-Django-Rest-Framework/commit/76eb764398628691a2be59bc87a541995913e2d7
 """
