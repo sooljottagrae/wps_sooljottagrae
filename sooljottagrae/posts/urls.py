8ff5193479b54b9a811bf652100a4d6816dd8a58
Doilad <tjalwled12@gmail.com>
2016-07-11 14:43:18 -0400

    Init PostListView

--- sooljottagrae/posts/urls.py
+++ sooljottagrae/posts/urls.py
@@ -5,6 +5,7 @@ from posts.views import *
 
 urlpatterns = [
 
-    url(r'new/$', PostCreateFormView.as_view(), name="create"),
-    url(r'confirm/$', PostCreateConfirmFormView.as_view(), name="confirm"),
+    url(r'^$', PostListView.as_view(), name="list"),
+    url(r'^new/$', PostCreateFormView.as_view(), name="create"),
+    url(r'^confirm/$', PostCreateConfirmFormView.as_view(), name="confirm"),
 ]
--- sooljottagrae/posts/views/__init__.py
+++ sooljottagrae/posts/views/__init__.py
@@ -1,1 +1,2 @@
 from .create import PostCreateFormView, PostCreateConfirmFormView
+from .list import PostListView
