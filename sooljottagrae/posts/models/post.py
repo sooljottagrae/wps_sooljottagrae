518c47a1042fe457d423f2abdf297828b3490590
Doilad <tjalwled12@gmail.com>
2016-07-12 03:09:27 -0400

    Make Post model and CRUD View * But, those are not tested!

--- sooljottagrae/posts/models/post.py
+++ sooljottagrae/posts/models/post.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+
 from users.models import MyUser
+
+
+class PostManager(models.Manager):
+    def public(self):
+        return self.filter(is_public=True)
 
 
 class Post(models.Model):
 
+    objects = PostManager()
     user = models.ForeignKey(MyUser)
     post_id = models.CharField(
         max_length=20,
@@ -24,6 +32,32 @@ class Post(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    
+    is_public = models.BooleanField(
+        default=True,
+    )
+    
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse(
+            "posts:detail",
+            kwargs={
+                "pk": self.id,
+            }
+        )
+    
+    def get_update_url(self):
+        return reverse(
+            "posts:update",
+            kwargs={
+                "post_id": self.id,
+            }
+        )
+    
+    def get_image_url(self):
+        if self.image:
+            return self.image.url
+        return "http://placehold.it/300x200"
+
--- sooljottagrae/posts/urls.py
+++ sooljottagrae/posts/urls.py
@@ -4,8 +4,11 @@ from posts.views import *
 
 
 urlpatterns = [
-
-    url(r'^$', PostListView.as_view(), name="list"),
-    url(r'^new/$', PostCreateFormView.as_view(), name="create"),
-    url(r'^confirm/$', PostCreateConfirmFormView.as_view(), name="confirm"),
+        url(r'^$', PostListView.as_view(), name="list"),
+        url(r'^(?P<pk>\d+)/$', PostDetailView.as_view(), name="detail"),
+        url(r'^new/$', PostNewView.as_view(), name="new"),
+        url(r'^create/$', PostCreateView.as_view(), name="create"),
+        url(r'^(?P<post_id>\d+)/edit/$', edit, name="edit"),
+        url(r'^(?P<post_id>\d+)/update/$', update, name="update"),
+        url(r'^(?P<post_id>\d+)/delete/$', delete, name="delete"),
 ]
