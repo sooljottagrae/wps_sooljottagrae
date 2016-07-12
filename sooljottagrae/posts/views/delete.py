e21f4d42c381aac31b771a676e86ba22971788ba
Doilad <tjalwled12@gmail.com>
2016-07-12 03:10:37 -0400

    Make Post model and CRUD Modify pep8 * this code is not tested yet

--- sooljottagrae/posts/views/delete.py
+++ sooljottagrae/posts/views/delete.py
@@ -7,7 +7,7 @@ from posts.models import Post
 def delete(request, post_id):
     post = Post.objects.get(id=post_id)
     post.delete()
-    
+
     return redirect(
         reverse("posts:list"),
     )
