0a6ba916213d3771c9d0299107aa0ec4882db381
alexKwonIsAwesome <khwalex@gmail.com>
2016-07-13 14:26:29 +0900

    update urls of apis:
    The login url was changed to jwt token obtain view. Because custom login
    view doesn't needed anymore. Just getting jwt token is an enough login
    process. Now login url is connecting to jwt token obtain view.

--- sooljottagrae/apis/urls/users.py
+++ sooljottagrae/apis/urls/users.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 
+from rest_framework_jwt.views import obtain_jwt_token
+
 from apis.views import *
 
 urlpatterns = [
         url(r'^$', UserListAPIView.as_view(), name="list"),
-        url(r'^login/$', UserLoginAPIView.as_view(), name="login"),
+        # url(r'^login/$', UserLoginAPIView.as_view(), name="login"),
+        url(r'^login/$', obtain_jwt_token),
         url(r'^signup/$', UserCreateAPIView.as_view(), name="signup"),
         url(r'^(?P<pk>\d+)/$', UserDetailAPIView.as_view(), name="detail"),
 ]
