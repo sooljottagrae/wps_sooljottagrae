72fe428bdd77f7391751e5e46d6448b0d0a1cadb
feanar729 <feanar729@gmail.com>
2016-07-13 16:23:09 +0900

    fixed comment __init__: testing fixed

---
+++ sooljottagrae/posts/models/comment.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+
+
+class Comment(models.Model):
+
+    user = models.ForeignKey(User)
+    post = models.ForeignKey("Post")
+
+    content = models.TextField()
+
+    def __str__(self):
+        return self.content
+
+    def get_absolute_url(self):
+        return reverse(
+            "posts:detail",
+            kwargs={
+                "pk": self.post.id,
+            },
+        ) + "#comment-" + str(self.id)
---
+++ sooljottagrae/posts/views/comments.py
@@ -0,0 +1,57 @@
+from django.views.generic import View
+from django.views.generic.edit import CreateView
+
+from django.shortcuts import redirect, render
+
+from posts.models import Post, Comment
+
+
+class CommentBaseView(View):
+    model = Comment
+
+
+class PostCommentCreateView(CommentBaseView, CreateView):
+    fields = [
+        "content",
+    ]
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.post = Post.objects.get(
+            id=self.kwargs.get("post_id"),
+        )
+
+        return super(PostCommentCreateView, self).form_valid(form)
+
+
+def comments_edit(request, post_id, comment_id):
+    post = Post.objects.get(id=post_id)
+    comment = post.comment_set.get(id=comment_id)
+
+    return render(
+        request,
+        "posts/comments_edit.html",
+        {
+            "post": post,
+            "comment": comment,
+        },
+    )
+
+
+def comments_update(request, post_id, comment_id):
+    post = Post.objects.get(id=post_id)
+    comment = post.comment_set.get(id=comment_id)
+
+    content = request.POST.get("content")
+    comment.content = content
+    comment.save()
+
+    return redirect(comment)
+
+
+def comments_delete(request, post_id, comment_id):
+    post = Post.objects.get(id=post_id)
+    comment = post.comment_set.get(id=comment_id)
+
+    comment.delete()
+    return redirect(post)
