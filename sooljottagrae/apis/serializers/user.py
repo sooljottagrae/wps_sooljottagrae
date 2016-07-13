06cf264a80691fd95e0a518de483044f5dc88f79
alexKwonIsAwesome <khwalex@gmail.com>
2016-07-13 14:29:38 +0900

    update serializers of user and post:
    As the User and Post model changed, the fields in meta had to be
    changed. username is no longer neede so changed to email.
    
    Also validation of email and password was included. Validating email
    whether it is already used one or not. Also taking password confirm for creating an user through the API.

--- sooljottagrae/apis/serializers/user.py
+++ sooljottagrae/apis/serializers/user.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from django.db.models import Q
 
 from rest_framework.serializers import (
+        EmailField,
         CharField,
         HyperlinkedIdentityField,
         ModelSerializer,
@@ -14,23 +15,43 @@ User = get_user_model()
 
 
 class UserCreateSerializer(ModelSerializer):
+    password = CharField(label="비밀번호")
+    password2 = CharField(label="비밀번호 확인")
 
     class Meta:
         model = User
         fields = [
-            "username",
+            "email",
+            "nickname",
             "password",
-            "phonenumber",
+            "password2",
         ]
         extra_kwargs = {"password": {"write_only": True}}
 
+    def validate_email(self, value):
+        data = self.get_initial()
+        email = data.get("email")
+
+        user_qs = User.objects.filter(email=email)
+        if user_qs.exists():
+            raise ValidationError("이미 사용중인 이메일입니다")
+        return value
+
+    def validate_password(self, value):
+        data = self.get_initial()
+        password1 = data.get("password")
+        password2 = value
+        if password1 != password2:
+            raise ValidationError("비밀번호가 일치하지 않습니다")
+        return value
+
     def create(self, validated_data):
-        username = validated_data['username']
+        email = validated_data['email']
         password = validated_data['password']
-        phonenumber = validated_data['phonenumber']
+        nickname = validated_data['nickname']
         user_obj = User(
-                username=username,
-                phonenumber=phonenumber,
+                email=email,
+                nickname=nickname,
             )
         user_obj.set_password(password)
         user_obj.save()
@@ -39,12 +60,13 @@ class UserCreateSerializer(ModelSerializer):
 
 class UserLoginSerializer(ModelSerializer):
     token = CharField(allow_blank=True, read_only=True)
-    username = CharField(required=False, allow_blank=True)
+    email = EmailField(required=False, allow_blank=True)
 
     class Meta:
         model = User
         fields = [
-                'username',
+                'email',
+                'nickname',
                 'password',
                 'token',
         ]
@@ -52,13 +74,13 @@ class UserLoginSerializer(ModelSerializer):
 
     def validate(self, data):
         user_obj = None
-        username = data.get("username", None)
+        email = data.get("email", None)
         password = data["password"]
-        if not username:
+        if not email:
             raise ValidationError("로그인하려면 사용자 이름을 입력하세요")
 
         user = User.objects.filter(
-                Q(username=username)
+                Q(email=email)
                 ).distinct()
         if user.exists() and user.count() == 1:
             user_obj = user.first()
@@ -79,7 +101,7 @@ class UserModelSerializer(ModelSerializer):
         model = User
         fields = [
                 "id",
-                "username",
+                "nickname",
+                "email",
                 "password",
-                "phonenumber",
         ]
