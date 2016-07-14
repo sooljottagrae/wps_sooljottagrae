da5d1b2cfbcb7de65e43900a655b675fe7d0f0f9
alexKwonIsAwesome <khwalex@gmail.com>
2016-07-14 16:49:33 +0900

    update files to meet pep8 rules:
    Minor changes. Such as unnecessary blanks.

--- sooljottagrae/users/models/user.py
+++ sooljottagrae/users/models/user.py
@@ -39,7 +39,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         blank=False,
         unique=True,
         default='')
-    
+
     avatar = models.ImageField(
         null=True,
         blank=True,
