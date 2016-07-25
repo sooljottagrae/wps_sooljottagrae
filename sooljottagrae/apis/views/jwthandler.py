ad24e7d2035ea1eef9c11b66e78c7c1291a9725d
alexKwonIsAwesome <khwalex@gmail.com>
2016-07-25 14:32:02 +0900

    update jwt token authentication returned datas:
    By overriding the jwt_response_payload_handler the returning data can be
    customized. So in this case I added serialized user data to be responsed
    when the login is successfully performed.
    
    So in response daya, the user data is serialized through the
    UserModelSerializer.

---
+++ sooljottagrae/apis/views/jwthandler.py
@@ -0,0 +1,10 @@
+from rest_framework_jwt.utils import jwt_response_payload_handler
+
+from apis.serializers.user import UserModelSerializer
+
+
+def jwt_response_payload_handler(token, user=None, request=None):
+    return {
+        'token': token,
+        'user': UserModelSerializer(user, context={'request':request}).data 
+    }
