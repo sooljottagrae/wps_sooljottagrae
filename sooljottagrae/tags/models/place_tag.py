74f14663ebdd5dcb4e6dfe21608570600130335b
Doilad <tjalwled12@gmail.com>
2016-07-27 02:11:07 -0400

    Complete Follow

--- sooljottagrae/tags/models/place_tag.py
+++ sooljottagrae/tags/models/place_tag.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.urlresolvers import reverse
 
 from posts.models import Post
+from users.models import User
 
 
 class PlaceTag(models.Model):
@@ -11,9 +12,14 @@ class PlaceTag(models.Model):
             )
 
     post_set = models.ManyToManyField(Post)
+    user_set = models.ManyToManyField(User)
+
+    @property
+    def full_name(self):
+        return "#{place_name}".format(place_name=self.place_name)
 
     def __str__(self):
-        return self.place_name
+        return self.full_name
 
     def get_absolute_api_url(self):
         return reverse(
